e2daf45bdfcd3431f2cdc8d95e550ca741d39bf2
Asif-Rezan <asifrezan@gmail.com>
2025-04-18 16:47:37 +0600

    update repository

--- lib/repository/auth_api/auth_http_api_repository.dart
+++ lib/repository/auth_api/auth_http_api_repository.dart
@@ -1,4 +1,3 @@
-
 import 'package:flutter/widgets.dart';
 
 import '../../data/network/network_api_services.dart';
@@ -14,16 +13,24 @@ class AuthHttpApiRepository implements AuthApiRepository {
   @override
   Future<bool> login(String email, String password) async {
     try {
-      final response = await _apiServices.postApi(
-        AppUrl.loginUrl,
-        {'username': email, 'password': password},
-      );
-      debugPrint('Login response: $response');
-      return response['success'] == true;
+      final response = await _apiServices.postApi(AppUrl.loginUrl, {
+        'username': email,
+        'password': password,
+      });
+      debugPrint('Login response repository: $response');
+
+      debugPrint('status code: ${response.statusCode}');
+
+      if (response['accessToken' !=  '']) {
+        return true;
+      } else {
+        debugPrint('Login failed: ${response.statusCode}');
+        return false;
+      }
     } catch (e) {
       debugPrint('Login failed: $e');
+      return false;
       rethrow;
     }
   }
-
 }
