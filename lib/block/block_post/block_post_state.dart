96617b0c9c9e56421d274b2bbf59fc9f79048228
backbencherstudio <bbsflutter@gmail.com>
2025-04-19 10:40:23 +0600

    update code

---
+++ lib/block/block_post/block_post_state.dart
@@ -0,0 +1,18 @@
+
+import '../../model/block_post/block_post_model.dart';
+
+abstract class PostState {}
+
+class PostInitial extends PostState {}
+
+class PostLoading extends PostState {}
+
+class PostLoaded extends PostState {
+  final List<PostModel> posts;
+  PostLoaded(this.posts);
+}
+
+class PostError extends PostState {
+  final String message;
+  PostError(this.message);
+}
